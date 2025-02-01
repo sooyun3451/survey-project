a359feaea7d4ed6f3dae79295ca18cfd87c7d897
sooyun3451 <sooyun020820@gmail.com>
2025-02-01 18:02:15 +0900

    20250201

--- 2024-05-07-project/src/main/survey/src/routes/auth/SnsSuccess.js
+++ 2024-05-07-project/src/main/survey/src/routes/auth/SnsSuccess.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import useAuthStore from "../../store/auth.store";
 
 export default function SnsSuccess() {
   const [cookies, setCookies] = useCookies(["token"]);
@@ -9,10 +10,10 @@ export default function SnsSuccess() {
   const [queryParam] = useSearchParams();
   const accessToken = queryParam.get("accessToken");
   const expiration = queryParam.get("expiration");
-  const userName = queryParam.get("userName");
-  const userCode = queryParam.get("userCode");
 
   const navigator = useNavigate();
+
+  const {login} = useAuthStore();
 
 
   useEffect(() => {
@@ -24,12 +25,13 @@ export default function SnsSuccess() {
         expires,
       });
 
-      localStorage.setItem("userName", userName);
-      localStorage.setItem("userCode", userCode);
+      login({
+        token: accessToken,
+      });
 
       navigator("/");
     } else navigator("/auth/signup/preson");
-  }, [accessToken, expiration, navigator, setCookies]);
+  }, [accessToken, expiration, navigator, setCookies, login]);
 
   return <></>;
 }
